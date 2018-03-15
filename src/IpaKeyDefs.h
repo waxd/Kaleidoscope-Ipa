7a5f48b7f11615067a7a7ec2d72af1946e62e071
David Wax <david.allen.wax@gmail.com>
2018-03-15 14:26:11 -0700

    Redefine the numbers for each IPA key based on a global offset.

--- src/IpaKeyDefs.h
+++ src/IpaKeyDefs.h
@@ -2,174 +2,186 @@
 
 #include <Kaleidoscope-Macros.h>
 
+// The number of keys defined in this file.
+#define IPA_KEY_COUNT 169
+
+// The IPA offset can be used to assign the block
+//   of Unicode keys to a seperate set of macros.
+#ifndef IPA_KEY_OFFSET
+#define IPA_KEY_OFFSET 256 - IPA_KEY_COUNT
+#endif
+
+// Used to apply a key in a keyboard layout.
+// e.g. IPA(Eng)
 #define IPA(n)   (Key){ n, KEY_FLAGS|SYNTHETIC|IS_MACRO}
 
-#define Lowercase_P 100
-#define Lowercase_B 101
-#define Lowercase_T 102
-#define Lowercase_D 103
-#define RightTail_T 104
-#define RightTail_D 105
-#define Lowercase_C 106
-#define Barred_Dotless_J 107
-#define Lowercase_K 108
-#define Opentail_G 109
-#define Lowercase_Q 110
-#define Small_Capital_G 111
-#define Glottal_Stop 112
-#define Lowercase_M 113
-#define Meng 114
-#define Lowercase_N 115
-#define RightTail_N 116
-#define LeftTail_N 117
-#define Eng 118
-#define Small_Capital_N 119
-#define Small_Capital_B 120
-#define Lowercase_R 121
-#define Small_Capital_R 122
-#define FishHook_R 123
-#define RightTail_R 124
-#define Phi 125
-#define Beta 126
-#define Lowercase_F 127
-#define Lowercase_V 128
-#define Theta 129
-#define Eth 130
-#define Lowercase_S 131
-#define Lowercase_Z 132
-#define Esh 133
-#define Ezh 134
-#define RightTail_S 135
-#define RightTail_Z 136
-#define Cedilla 137
-#define CurlyTail_J 138
-#define Lowercase_X 139
-#define Gamma 140
-#define Chi 141
-#define Inverted_Small_Capital_R 142
-#define Barred_H 143
-#define Reversed_Glottal_Stop 144
-#define Lowercase_H 145
-#define Hooktop_H 146
-#define Belted_L 147
-#define Lezh 148
-#define Cursive_V 149
-#define Turned_R 150
-#define RightTail_Turned_R 151
-#define Lowercase_J 152
-#define RightLeg_Turned_M 153
-#define Lowercase_L 154
-#define RightTail_L 155
-#define Turned_Y 156
-#define Small_Capital_L 157
-#define Hooktop_B 158
-#define Hooktop_D 159
-#define Hooktop_Barred_Dotless_J 160
-#define Hooktop_G 161
-#define Hooktop_Small_Capital_G 162
-#define Turned_W 163
-#define Lowercase_W 164
-#define Turned_H 165
-#define Small_Capital_H 166
-#define Barred_Reversed_Glottal_Stop 167
-#define Reversed_Glottal_Stop 168
-#define Hooktop_Heng 169
-#define Bulls_Eye 170
-#define Pipe 171
-#define Excalmation_Point 172
-#define DoubleBarred_Pipe 173
-#define Double_Pipe 174
-#define Turned_LongLeg_R 175
-#define CurlyTail_C 176
-#define CurlyTail_Z 177
-#define RightHook_V 178
-#define Tilde_L 179
-#define Lowercase_I 180
-#define Lowercase_E 181
-#define Epsilon 182
-#define Lowercase_A 183
-#define Script_A 184
-#define Open_O 185
-#define Lowercase_O 186
-#define Lowercase_U 187
-#define Lowercase_Y 188
-#define Slashed_O 189
-#define Lowercase_Ethel 190
-#define Small_Capital_Ethel 191
-#define Turned_Script_A 192
-#define Turned_V 193
-#define Rams_Horns 194
-#define Turned_M 195
-#define Barred_L 196
-#define Barred_U 197
-#define Small_Capital_I 198
-#define Small_Capital_Y 199
-#define Upsilon 200
-#define Schwa 201
-#define Barred_O 202
-#define Turned_A 203
-#define Ash 204
-#define Reversed_Epsilon 205
-#define RightHook_Schwa 206
-#define Closed_Reversed_Epsilon 207
-#define Reversed_E 208
-#define Apostrophe 209
-#define UnderRing 210
-#define OverRing 211
-#define Subscript_Wedge 212
-#define Superscript_H 213
-#define Subscript_Umlaut 214
-#define Subscript_Tilde 215
-#define Subscript_Seagull 216
-#define Subscript_Bridge 217
-#define Inverted_Subscript_Bridge 218
-#define Subscript_Square 219
-#define Subscript_Right_HalfRing 220
-#define Subscript_Left_HalfRing 221
-#define Subscript_Plus 222
-#define UnderBar 223
-#define Umlaut 224
-#define OverCross 225
-#define Advancing_Sign 226
-#define Retracting_Sign 227
-#define RightHook 
-#define Superscript_W 228
-#define Superscript_J 229
-#define Superscript_Gamma 230
-#define Superscript_Reversed_Glottal_Stop 231
-#define Superscript_Tilde 232
-#define Superscript_N 233
-#define Superscript_L 234
-#define Corner 235
-#define Raising_Sign 236
-#define Lowering_Sign 237
-#define Syllabicty_Mark 238
-#define Subscript_Arch 239
-#define Top_Tie_Bar 240
-#define Bottom_Tie_Bar 241
-#define Superior_Vertical_Stroke 242
-#define Inferior_Vertical_Stroke 243
-#define Length_Mark 244
-#define HalfLength_Mark 245
-#define Breve 246
-#define Period 247
-#define Vertical_Line 248
-#define Double_Vertical_Line 249
-#define Upward_Diagonal_Arrow 250
-#define Downward_Diagonal_Arrow 251
-#define Double_Acute_Accent 252
-#define Acute_Accent 253
-#define Macron 254
-#define Grave_Accent 255
-#define Double_Grave_Accent 99
-#define Down_Arrow 98
-#define Up_Arrow 97
-#define ExtraHigh_Tone_Letter 96
-#define High_Tone_Letter 95
-#define Mid_Tone_Letter 94
-#define Low_Tone_Letter 93
-#define ExtraLow_Tone_Letter 92
-#define Wedge 91
-#define Circumflex 90
-#define Macron_Plus_Acute_Accent 89
-#define Grave_Accent_Plus_Macron 88
+
+#define Lowercase_P IPA_KEY_OFFSET + 0
+#define Lowercase_B IPA_KEY_OFFSET + 1
+#define Lowercase_T IPA_KEY_OFFSET + 2
+#define Lowercase_D IPA_KEY_OFFSET + 3
+#define RightTail_T IPA_KEY_OFFSET + 4
+#define RightTail_D IPA_KEY_OFFSET + 5
+#define Lowercase_C IPA_KEY_OFFSET + 6
+#define Barred_Dotless_J IPA_KEY_OFFSET + 7
+#define Lowercase_K IPA_KEY_OFFSET + 8
+#define Opentail_G IPA_KEY_OFFSET + 9
+#define Lowercase_Q IPA_KEY_OFFSET + 10
+#define Small_Capital_G IPA_KEY_OFFSET + 11
+#define Glottal_Stop IPA_KEY_OFFSET + 12
+#define Lowercase_M IPA_KEY_OFFSET + 13
+#define Meng IPA_KEY_OFFSET + 14
+#define Lowercase_N IPA_KEY_OFFSET + 15
+#define RightTail_N IPA_KEY_OFFSET + 16
+#define LeftTail_N IPA_KEY_OFFSET + 17
+#define Eng IPA_KEY_OFFSET + 18
+#define Small_Capital_N IPA_KEY_OFFSET + 19
+#define Small_Capital_B IPA_KEY_OFFSET + 20
+#define Lowercase_R IPA_KEY_OFFSET + 21
+#define Small_Capital_R IPA_KEY_OFFSET + 22
+#define FishHook_R IPA_KEY_OFFSET + 23
+#define RightTail_R IPA_KEY_OFFSET + 24
+#define Phi IPA_KEY_OFFSET + 25
+#define Beta IPA_KEY_OFFSET + 26
+#define Lowercase_F IPA_KEY_OFFSET + 27
+#define Lowercase_V IPA_KEY_OFFSET + 28
+#define Theta IPA_KEY_OFFSET + 29
+#define Eth IPA_KEY_OFFSET + 30
+#define Lowercase_S IPA_KEY_OFFSET + 31
+#define Lowercase_Z IPA_KEY_OFFSET + 32
+#define Esh IPA_KEY_OFFSET + 33
+#define Ezh IPA_KEY_OFFSET + 34
+#define RightTail_S IPA_KEY_OFFSET + 35
+#define RightTail_Z IPA_KEY_OFFSET + 36
+#define Cedilla IPA_KEY_OFFSET + 37
+#define CurlyTail_J IPA_KEY_OFFSET + 38
+#define Lowercase_X IPA_KEY_OFFSET + 39
+#define Gamma IPA_KEY_OFFSET + 40
+#define Chi IPA_KEY_OFFSET + 41
+#define Inverted_Small_Capital_R IPA_KEY_OFFSET + 42
+#define Barred_H IPA_KEY_OFFSET + 43
+#define Reversed_Glottal_Stop IPA_KEY_OFFSET + 44
+#define Lowercase_H IPA_KEY_OFFSET + 45
+#define Hooktop_H IPA_KEY_OFFSET + 46
+#define Belted_L IPA_KEY_OFFSET + 47
+#define Lezh IPA_KEY_OFFSET + 48
+#define Cursive_V IPA_KEY_OFFSET + 49
+#define Turned_R IPA_KEY_OFFSET + 50
+#define RightTail_Turned_R IPA_KEY_OFFSET + 51
+#define Lowercase_J IPA_KEY_OFFSET + 52
+#define RightLeg_Turned_M IPA_KEY_OFFSET + 53
+#define Lowercase_L IPA_KEY_OFFSET + 54
+#define RightTail_L IPA_KEY_OFFSET + 55
+#define Turned_Y IPA_KEY_OFFSET + 56
+#define Small_Capital_L IPA_KEY_OFFSET + 57
+#define Hooktop_B IPA_KEY_OFFSET + 58
+#define Hooktop_D IPA_KEY_OFFSET + 59
+#define Hooktop_Barred_Dotless_J IPA_KEY_OFFSET + 60
+#define Hooktop_G IPA_KEY_OFFSET + 61
+#define Hooktop_Small_Capital_G IPA_KEY_OFFSET + 62
+#define Turned_W IPA_KEY_OFFSET + 63
+#define Lowercase_W IPA_KEY_OFFSET + 64
+#define Turned_H IPA_KEY_OFFSET + 65
+#define Small_Capital_H IPA_KEY_OFFSET + 66
+#define Barred_Reversed_Glottal_Stop IPA_KEY_OFFSET + 67
+#define Reversed_Glottal_Stop IPA_KEY_OFFSET + 68
+#define Hooktop_Heng IPA_KEY_OFFSET + 69
+#define Bulls_Eye IPA_KEY_OFFSET + 70
+#define Pipe IPA_KEY_OFFSET + 71
+#define Excalmation_Point IPA_KEY_OFFSET + 72
+#define DoubleBarred_Pipe IPA_KEY_OFFSET + 73
+#define Double_Pipe IPA_KEY_OFFSET + 74
+#define Turned_LongLeg_R IPA_KEY_OFFSET + 75
+#define CurlyTail_C IPA_KEY_OFFSET + 76
+#define CurlyTail_Z IPA_KEY_OFFSET + 77
+#define RightHook_V IPA_KEY_OFFSET + 78
+#define Tilde_L IPA_KEY_OFFSET + 79
+#define Lowercase_I IPA_KEY_OFFSET + 80
+#define Lowercase_E IPA_KEY_OFFSET + 81
+#define Epsilon IPA_KEY_OFFSET + 82
+#define Lowercase_A IPA_KEY_OFFSET + 83
+#define Script_A IPA_KEY_OFFSET + 84
+#define Open_O IPA_KEY_OFFSET + 85
+#define Lowercase_O IPA_KEY_OFFSET + 86
+#define Lowercase_U IPA_KEY_OFFSET + 87
+#define Lowercase_Y IPA_KEY_OFFSET + 88
+#define Slashed_O IPA_KEY_OFFSET + 89
+#define Lowercase_Ethel IPA_KEY_OFFSET + 90
+#define Small_Capital_Ethel IPA_KEY_OFFSET + 91
+#define Turned_Script_A IPA_KEY_OFFSET + 92
+#define Turned_V IPA_KEY_OFFSET + 93
+#define Rams_Horns IPA_KEY_OFFSET + 94
+#define Turned_M IPA_KEY_OFFSET + 95
+#define Barred_L IPA_KEY_OFFSET + 96
+#define Barred_U IPA_KEY_OFFSET + 97
+#define Small_Capital_I IPA_KEY_OFFSET + 98
+#define Small_Capital_Y IPA_KEY_OFFSET + 99
+#define Upsilon IPA_KEY_OFFSET + 100
+#define Schwa IPA_KEY_OFFSET + 101
+#define Barred_O IPA_KEY_OFFSET + 102
+#define Turned_A IPA_KEY_OFFSET + 103
+#define Ash IPA_KEY_OFFSET + 104
+#define Reversed_Epsilon IPA_KEY_OFFSET + 105
+#define RightHook_Schwa IPA_KEY_OFFSET + 106
+#define Closed_Reversed_Epsilon IPA_KEY_OFFSET + 107
+#define Reversed_E IPA_KEY_OFFSET + 108
+#define Apostrophe IPA_KEY_OFFSET + 109
+#define UnderRing IPA_KEY_OFFSET + 110
+#define OverRing IPA_KEY_OFFSET + 111
+#define Subscript_Wedge IPA_KEY_OFFSET + 112
+#define Superscript_H IPA_KEY_OFFSET + 113
+#define Subscript_Umlaut IPA_KEY_OFFSET + 114
+#define Subscript_Tilde IPA_KEY_OFFSET + 115
+#define Subscript_Seagull IPA_KEY_OFFSET + 116
+#define Subscript_Bridge IPA_KEY_OFFSET + 117
+#define Inverted_Subscript_Bridge IPA_KEY_OFFSET + 118
+#define Subscript_Square IPA_KEY_OFFSET + 119
+#define Subscript_Right_HalfRing IPA_KEY_OFFSET + 120
+#define Subscript_Left_HalfRing IPA_KEY_OFFSET + 121
+#define Subscript_Plus IPA_KEY_OFFSET + 122
+#define UnderBar IPA_KEY_OFFSET + 123
+#define Umlaut IPA_KEY_OFFSET + 124
+#define OverCross IPA_KEY_OFFSET + 125
+#define Advancing_Sign IPA_KEY_OFFSET + 126
+#define Retracting_Sign IPA_KEY_OFFSET + 127
+#define RightHook IPA_KEY_OFFSET + 128
+#define Superscript_W IPA_KEY_OFFSET + 129
+#define Superscript_J IPA_KEY_OFFSET + 130
+#define Superscript_Gamma IPA_KEY_OFFSET + 131
+#define Superscript_Reversed_Glottal_Stop IPA_KEY_OFFSET + 132
+#define Superscript_Tilde IPA_KEY_OFFSET + 133
+#define Superscript_N IPA_KEY_OFFSET + 134
+#define Superscript_L IPA_KEY_OFFSET + 135
+#define Corner IPA_KEY_OFFSET + 136
+#define Raising_Sign IPA_KEY_OFFSET + 137
+#define Lowering_Sign IPA_KEY_OFFSET + 138
+#define Syllabicty_Mark IPA_KEY_OFFSET + 139
+#define Subscript_Arch IPA_KEY_OFFSET + 140
+#define Top_Tie_Bar IPA_KEY_OFFSET + 141
+#define Bottom_Tie_Bar IPA_KEY_OFFSET + 142
+#define Superior_Vertical_Stroke IPA_KEY_OFFSET + 143
+#define Inferior_Vertical_Stroke IPA_KEY_OFFSET + 144
+#define Length_Mark IPA_KEY_OFFSET + 145
+#define HalfLength_Mark IPA_KEY_OFFSET + 146
+#define Breve IPA_KEY_OFFSET + 147
+#define Period IPA_KEY_OFFSET + 148
+#define Vertical_Line IPA_KEY_OFFSET + 149
+#define Double_Vertical_Line IPA_KEY_OFFSET + 150
+#define Upward_Diagonal_Arrow IPA_KEY_OFFSET + 151
+#define Downward_Diagonal_Arrow IPA_KEY_OFFSET + 152
+#define Double_Acute_Accent IPA_KEY_OFFSET + 153
+#define Acute_Accent IPA_KEY_OFFSET + 154
+#define Macron IPA_KEY_OFFSET + 155
+#define Grave_Accent IPA_KEY_OFFSET + 156
+#define Double_Grave_Accent IPA_KEY_OFFSET + 157
+#define Down_Arrow IPA_KEY_OFFSET + 158
+#define Up_Arrow IPA_KEY_OFFSET + 159
+#define ExtraHigh_Tone_Letter IPA_KEY_OFFSET + 160
+#define High_Tone_Letter IPA_KEY_OFFSET + 161
+#define Mid_Tone_Letter IPA_KEY_OFFSET + 162
+#define Low_Tone_Letter IPA_KEY_OFFSET + 163
+#define ExtraLow_Tone_Letter IPA_KEY_OFFSET + 164
+#define Wedge IPA_KEY_OFFSET + 165
+#define Circumflex IPA_KEY_OFFSET + 166
+#define Macron_Plus_Acute_Accent IPA_KEY_OFFSET + 167
+#define Grave_Accent_Plus_Macron IPA_KEY_OFFSET + 168
